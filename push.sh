740441ee1ce2d8a814c7b07e819d46e7501af21c
hollyb18 <Holly.Bate@nationwide.co.uk>
2020-09-30 14:37:57 +0100

    corrected name of tagged image

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 sudo docker login -u admin -p'Pa$$w0rd' localhost:8082
 sudo docker tag trio-task-pipeline_mysql:latest localhost:8082/trio-task-pipeline_mysql:latest
-sudo docker tag trio-task-pipeline_flask-app:latest localhost:8082/trio-task-pipeline_flask-db:latest
+sudo docker tag trio-task-pipeline_flask-app:latest localhost:8082/trio-task-pipeline_flask-app:latest
 sudo docker push localhost:8082/trio-task-pipeline_mysql:latest
 sudo docker push localhost:8082/trio-task-pipeline_flask-app:latest
