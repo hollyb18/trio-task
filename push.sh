8917ed57db7fab74f91010c726481198bf9e0af1
hollyb18 <Holly.Bate@nationwide.co.uk>
2020-09-30 14:32:57 +0100

    removed read for password entry

--- push.sh
+++ push.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-echo 'Please enter your nexus password:'
-read password
-sudo docker login -u admin -p${password} localhost:8082
+sudo docker login -u admin -p'Pa$$w0rd' localhost:8082
 sudo docker tag trio-task-pipeline_mysql:latest localhost:8082/trio-task-pipeline_mysql:latest
 sudo docker tag trio-task-pipeline_flask-app:latest localhost:8082/trio-task-pipeline_flask-db:latest
 sudo docker push localhost:8082/trio-task-pipeline_mysql:latest
